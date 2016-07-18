dfb62ba650be2445ae953f5e4b9d59b62f9acf51
Ed <ek2000@gmail.com>
2016-07-18 09:24:45 -0500

    Adds search functionality

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 var app = angular.module("redditApp", []);
 
-app.controller('UserPosts', function($scope){
+app.controller('UserPosts', function($scope, orderByFilter){
   $scope.postList = [];
   $scope.post = {};
+  $scope.sort = ['Vote', 'Date', 'Title'];
   $scope.addPost = function(){
+    $scope.post.vote = 0;
+    $scope.post.time = moment().calendar();
     var newPost = {};
     newPost.author = $scope.post.author;
     newPost.title = $scope.post.title;
     newPost.description = $scope.post.description;
     newPost.img = $scope.post.img;
     newPost.time = $scope.post.time;
-
+    newPost.vote = $scope.post.vote;
     $scope.postList.push(newPost);
+    $scope.postList = orderByFilter($scope.postList, $scope.post.vote);
   }
+
+  $scope.thumbsUp = function(){
+    this.post.vote++;
+  }
+
+  $scope.thumbsDown = function(){
+    this.post.vote--;
+  }
+
+  $scope.sortVal = function(val){
+    $scope.filter = val;
+    $scope.postList = orderByFilter($scope.postList, $scope.post.vote);
+  }
+
+
   // $scope.postList = [{
   //   author: "Ed",
   //   title: "Seattle",
